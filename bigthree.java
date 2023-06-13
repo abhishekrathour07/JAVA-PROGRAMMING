e55542969fdff091c7c7e82e5223b81e38755d4d
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-06-13 23:30:21 +0530

    a

---
+++ bigthree.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bigThree {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three number");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println("A is largest");
+            } else {
+                System.out.println("C is largest");
+            }
+        } else {
+            if (b > c) {
+                System.out.println("B is largest");
+            } else {
+                System.out.println("C is Largest");
+            }
+        }
+
+        input.close();
+    }
+}

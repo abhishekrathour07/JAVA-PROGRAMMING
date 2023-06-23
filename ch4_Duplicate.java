8d1ecffb966c775e95ce5abe559f5231dd66b7eb
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-06-24 02:39:58 +0530

    array dublicate

--- ch4_Duplicate.java
+++ ch4_Duplicate.java
@@ -1,15 +1,35 @@
 import java.util.Scanner;
+
 public class ch4_Duplicate {
    public static void main(String[] args) {
-    Scanner input =new Scanner(System.in);
-    int i;
-    System.out.println("Enter the size of an array");
-    int n = input.nextInt();
-    System.out.println("Enter the element in arrsy");
-    int arr[] = new int[n];
-    for(i=0;i<n;i++){
-            
-    }
+      Scanner input = new Scanner(System.in);
+      int i;
+      System.out.println("Enter the size of an array");
+      int n = input.nextInt();
+      System.out.println("Enter the element in arrsy");
+      int arr[] = new int[n];
+      for (i = 0; i < n; i++) {
+         arr[i] = input.nextInt();
+      }
+      System.out.println("Array element are :- ");
+      for (i = 0; i < n; i++) {
+         System.out.printf("%d\t", arr[i]);
+      }
+      for (i = 0; i < n; i++) {
+         for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+               for (int k = j; k < n; k++) {
+                  arr[k] = arr[k + 1];
+               }
+               n--;
+               j--;
+            }
+         }
+      }
+      System.out.println("Array element after removing dublicate is : ");
+      for (i = 0; i < n; i++) {
+         System.out.printf("%d\t", arr[i]);
+      }
+
    }
 }
-

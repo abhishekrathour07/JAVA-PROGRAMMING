a780f4e87df6a61f521fb93a9dc8918f5fdb2f12
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-07-04 02:15:43 +0530

    up Anc down Casting :

--- ch7_UpcastDowncast.java
+++ ch7_UpcastDowncast.java
@@ -1,48 +1,49 @@
-class Parent {   
+class Parent {
     String name;
-    
-    void showMessage()   
-    {   
-        System.out.println("Parent method is called");   
-    }   
-    void show(){
+
+    void showMessage() {
+        System.out.println("Parent method is called");
+    }
+
+    void show() {
         System.out.println("This is showing Something");
     }
-}   
-    
-class Child extends Parent {   
-    int age;   
-    
-    // @Override  
-    void showMessage()   
-    {   
-        System.out.println("Child method is called");   
-    }   
-      void showMessage1()   
-    {   
-        System.out.println("Child method is called without overriding");   
-    }   
-}   
-    
-public class ch7_UpcastDowncast{  
-    
-    public static void main(String[] args)   
-    {   
-        Parent p = new Child();  
-        p.name = "Shubham";  
-        //   p.age = 18; --> its gives a error because in upcasting we cannot access child class 
+}
+
+class Child extends Parent {
+    int age;
+
+    // @Override
+    void showMessage() {
+        System.out.println("Child method is called");
+    }
+
+    void showMessage1() {
+        System.out.println("Child method is called without overriding");
+    }
+}
+
+public class ch7_UpcastDowncast {
+
+    public static void main(String[] args) {
+        Parent p = new Child();
+        p.name = "Shubham";
+        // p.age = 18; --> its gives a error because in upcasting we cannot access child
+        // class
         p.showMessage();
-        // p.showMessage1();  --> Error bcz we cannot access child calss element or mrthods 
-        // Performing Downcasting Implicitly   
-        //Child c = new Parent(); // it gives compile-time error   
-        // Performing Downcasting Explicitly   
-        Child c = (Child)p;   
-    
-        c.age = 18;   
-        System.out.println(c.name);   
-        System.out.println(c.age);   
-        c.showMessage();   
+        // p.showMessage1(); --> Error bcz we cannot access child calss element or
+        // mrthods
+
+        // Performing Downcasting Implicitly
+        // Child c = new Parent(); // it gives compile-time error
+        // Performing Downcasting Explicitly
+        Child c = (Child) p;
+
+        c.age = 18;
+        System.out.println(c.name);
+        System.out.println(c.age);
+        c.showMessage();
         c.showMessage1();
         c.show();
-    }   
-}  
+    }
+}

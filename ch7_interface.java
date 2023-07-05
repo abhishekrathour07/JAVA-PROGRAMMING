d96b6fdeec90b7b50c110e9c484e399dfdef929e
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-07-05 15:38:37 +0530

    Interface

--- ch7_interface.java
+++ ch7_interface.java
@@ -1,12 +1,14 @@
-interface camera{
-       void takingpic();
+interface camera {
+    void takingpic();
 
-       void msgwhiletaking();
-       default void mereram(){
-        System.out.println("Bolo Jai shree ram");
-       }
+    void msgwhiletaking();
+
+    default void def() {
+        System.out.println("Making default method in interface");
+    }
 }
-class saurabh implements camera{
+
+class inter implements camera {
 
     @Override
     public void takingpic() {
@@ -16,18 +18,18 @@ class saurabh implements camera{
     @Override
     public void msgwhiletaking() {
         System.out.println("Smile please...");
-        
+
     }
 
-    
 }
+
 public class ch7_interface {
     public static void main(String[] args) {
-        saurabh s = new saurabh();
+        inter s = new inter();
         s.takingpic();
         s.msgwhiletaking();
-       
+        s.def();
+
     }
-    
+
 }
-  

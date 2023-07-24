2f89cf9ec225e8c41b2c31c5e5982e3a2232006b
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-07-24 14:32:37 +0530

    Negative sign for array

--- ch9_Labprogram11.java
+++ ch9_Labprogram11.java
@@ -4,7 +4,7 @@ public class ch9_Labprogram11 {
         try {
             int array[] = new int[-1];
         } catch (Exception obj) {
-            obj.printStackTrace();
+            System.out.println(obj);
         }
         System.out.println("Exception caught and continue execution");
     }

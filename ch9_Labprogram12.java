dc71798a7c02d25489c857d5d477da3109da0955
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-07-25 23:11:43 +0530

    JAva Lab

---
+++ ch9_Labprogram12.java
@@ -0,0 +1,16 @@
+public class ch9_Labprogram12 {
+    public static void main(String args[]) {
+        String city = null;
+        try {
+            if (city.equals("BANGALORE"))
+                System.out.println("Equal");
+            else
+                System.out.println("Not Equal");
+        } catch (Exception e) {
+            System.out.println("Null pointer exception caught");
+        } finally {
+            System.out.println("finally block will be always executed");
+        }
+    }
+}
+ 

6de3ae4f5b0cb012104b305c8f45d7180636dc1d
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2023-07-29 14:11:54 +0530

    Menubar

---
+++ ch11_MenuDemo.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+
+public class ch11_MenuDemo {
+    public static void main(String[] args) {
+        Frame f = new Frame("Frame");
+        MenuBar m = new MenuBar();
+        Menu file = new Menu("files");
+        Menu edit = new Menu("Edit");
+        Menu view = new Menu("View");
+        m.add(file);
+        m.add(edit);
+        m.add(view);
+        MenuItem a1 = new MenuItem("new");
+        MenuItem a2 = new MenuItem("Save");
+        MenuItem a3 = new MenuItem("Open");
+        MenuItem a4 = new MenuItem("Save as");
+        MenuItem b1 = new MenuItem("Cut");
+        MenuItem b2 = new MenuItem("Copy");
+        MenuItem b3 = new MenuItem("Paste");
+        MenuItem c1 = new MenuItem("Show");
+        MenuItem c2 = new MenuItem("Find");
+        file.add(a1);
+        file.add(a2);
+        file.add(a3);
+        file.add(a4);
+
+        edit.add(b1);
+        edit.add(b2);
+        edit.add(b3);
+
+        view.add(c1);
+        view.add(c2);
+        
+        f.setMenuBar(m);
+        f.setVisible(true);
+        f.setSize(150, 250);
+
+    }
+}
--- practice2.java
+++ practice2.java
@@ -1,39 +1,15 @@
-import java.awt.*;
-import java.awt.event.*;
-
-public class practice2 {
-    public static void main(String[] args) {
-        // Create the main frame
-        Frame frame = new Frame("BorderLayout Example");
-
-        // Add a WindowListener to handle the window close event
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                // Close the frame and exit the application
-                frame.dispose();
-                System.exit(0);
-            }
-        });
-
-        // Create components
-        Button button1 = new Button("North");
-        Button button2 = new Button("South");
-        Button button3 = new Button("East");
-        Button button4 = new Button("West");
-        Button button5 = new Button("Center");
-
-        // Set the layout manager for the frame
-        frame.setLayout(new BorderLayout());
-
-        // Add components to the frame with specified regions
-        frame.add(button1, BorderLayout.NORTH);
-        frame.add(button2, BorderLayout.SOUTH);
-        frame.add(button3, BorderLayout.EAST);
-        frame.add(button4, BorderLayout.WEST);
-        frame.add(button5, BorderLayout.CENTER);
-
-        // Set the frame size and make it visible
-        frame.setSize(300, 150);
-        frame.setVisible(true);
+class M{
+    void display(){
+        System.out.println("m");
+    }
+}
+class n extends M{
+    void show(){
+        System.out.println("THis is show");
+    }
+}
+class b extends M{
+    void show1(){
+        System.out.println("Show1");
     }
 }
